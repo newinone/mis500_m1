6f0aeb50833edb08d3b60787c52c622d1590e01c
Ingrid Niu <yniu@pason.com>
2019-02-14 13:06:36 -0700

    lynda.com c2

---
+++ src/main/python/removeDuplicates.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+
+series_obj = Series(
+    np.arange(8),
+    index=[
+        'row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6', 'row 7', 'row 8'
+    ])
+print(series_obj['row 2'])
+print(series_obj[1])
+
+missing = np.nan
+
+series_obj = Series(
+    ['row 1', 'row 2', missing, 'row 4', 'row 5', missing, 'row 7', 'row 8'])
+print(series_obj)
+# list of missing values True/False
+print(series_obj.isnull())
+
+np.random.seed(25)
+# Generate 6x6 matrix
+DF_obj = DataFrame(np.random.randn(36).reshape(6, 6))
+print(DF_obj)
+
+# Change some value to missing
+DF_obj.ix[3:5, 0] = missing
+DF_obj.ix[1:4, 0] = missing
+print(DF_obj)
+
+# Fill 0 to missing values
+filled_DF = DF_obj.fillna(0)
+print(filled_DF)
+
+# Fill column 0 with 0.1 and column 5 with 1.25
+filled_DF = DF_obj.fillna({0: 0.1, 5: 1.25})
+print(filled_DF)
+
+# Forward fill(copy privious value to NaN)
+filled_DF = DF_obj.fillna(method='ffill')
+print(filled_DF)
+
+
+
+
